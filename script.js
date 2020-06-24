4021cf89e17068c3198689534a2114eb4c0ad40b
jonas-for-loop <jonas-fluck@gmx.de>
2020-06-24 14:33:15 +0200

    gg

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-var myBarChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: data,
-    options: options
-});
-data: {
-    datasets: [{
-        barPercentage: 0.5,
-        barThickness: 6,
-        maxBarThickness: 8,
-        minBarLength: 2,
-        data: [10, 20, 30, 40, 50, 60, 70]
-    }]
-};
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
