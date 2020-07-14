fa544c9f073fd6b156d75f4bfbf73907c407ec61
jonas-for-loop <jonas-fluck@gmx.de>
2020-07-14 18:38:40 +0200

    lot of changes

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+// JavaScript source code
+function popUp() {
+    // Get the modal
+    var modalpopUP = document.getElementById("unserText");
+    modalpopUP.style.display = "block";
+
+
+    var img = document.getElementById("myImg");
+
+    // Get the <span> element that closes the modal
+    //var spanPopUp = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    img.onmouseleave = function closePopUp() {
+        modalpopUP.style.display = "none";
+    }
+}
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
